cfd9fc2969921bb37fa4d506c22b0659f3aef0d7
Mario Alberto Alvarado Fernández <malvaradofdz98@gmail.com>
2020-06-28 20:59:38 -0500

    Viex correction + Vuex installed correctly

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,12 @@ window.Vue = require('vue');
 //Import of manually created file for Vuetify setup
 import Vuetify from "../plugins/vuetify";
 
+import store from "./store";
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 const app = new Vue({
     vuetify: Vuetify,
+    store,
     el: '#app'
 });
